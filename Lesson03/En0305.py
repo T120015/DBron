f7762cd1151b16b953b463e83598393723d766db
T120015 <T120015@ed.sus.ac.jp>
2021-12-23 15:22:06 +0900

    2021/12/23-15:22:05

--- Lesson03/En0305.py
+++ Lesson03/En0305.py
@@ -1,17 +1,25 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import japanize_matplotlib
 from mydblib import my_select as slc
 
 frm = "post_office"
 city = "茅野市"
-prefecture = ["長野県","新潟県","富山県","石川県","福井県"]
+prefecture = ["長野県", "新潟県", "富山県", "石川県", "福井県"]
 
 sqlstring = f"""
-    SELECT *
+    SELECT prefecture, COUNT(prefecture) AS pcnt
     FROM {frm}
-    AND prefecture IN('{prefecture}')
+    WHERE prefecture IN('{prefecture[0]}','{prefecture[1]}','{prefecture[2]}','{prefecture[3]}','{prefecture[4]}')
+    GROUP BY prefecture
     ;
 """
+
 postNum = slc(sqlstring)
-#index, row = df.iterrows()１行ずつ繰り返す
-for index, row in postNum.iterrows():
-  print("{} {} {}".format(row["postnumber"], row["officename"], row["area"]))
+print(postNum)
+# make graph
+plt.bar(range(postNum["pregecture"]), postNum["pcnt"],ticklabel=postNum["prefecture"])
+plt.title("北信越地方県別事業所数")
+plt.xlabel("県名")
+plt.ylabel("登録数便番号数")
+plt.savefig("./img/En0305.png")
