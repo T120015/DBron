fbcd2a8f03c4198a720659e4ba1fcd4ea64ff7ad
T120015 <T120015@ed.sus.ac.jp>
2021-12-23 14:55:02 +0900

    2021/12/23-14:55:01

--- Lesson03/En0304.py
+++ Lesson03/En0304.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import mydblib as mdb
 
-frm = "post_area"
+frm = "post_office"
 city = "茅野市"
 prefecture = "長野県"
 
 sqlstring = f"""
     SELECT *
     FROM {frm}
-    WHERE city LIKE '%{city}%'
+    WHERE city ='%{city}%'
     AND prefecture = '{prefecture}'
     ;
 """
