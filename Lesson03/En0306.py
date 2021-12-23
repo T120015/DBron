3d9eddd9cca5f97feefe19f03caffffa77e3b7d8
T120015 <T120015@ed.sus.ac.jp>
2021-12-23 16:30:44 +0900

    2021/12/23-16:30:43

--- Lesson03/En0306.py
+++ Lesson03/En0306.py
@@ -1,12 +1,18 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-from mydblib import my_select as mslc
+from mydblib import my_select as slc
+
+
 
 def main():
   data = """
-  SELECT *
-  FROM
+  SELECT answer1, COUNT(answer1) AS cnt
+  FROM quest
   """
+
+  pos_num = slc(data)
+
+  print(pos_num)
 
 if __name__ == "__main__":
   main()
