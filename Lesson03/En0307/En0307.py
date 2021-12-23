1f7d04b53a148c8c261c143d2a0fedcb52096fbc
T120015 <T120015@ed.sus.ac.jp>
2021-12-23 16:54:51 +0900

    2021/12/23-16:54:50

--- Lesson03/En0307/En0307.py
+++ Lesson03/En0307/En0307.py
@@ -1,21 +1,24 @@
-from flask import Flask as flk,render_template as rndtmp
+from flask import Flask as flk, render_template as rndtmp
 from numpy.lib.function_base import append
 from sympy import isprime as ip
 import numpy as np
 
 app = flk(__name__)
 
+
 @app.route("/")
 def top():
-  return "ようこそ素数の世界へ"
+    return "ようこそ素数の世界へ"
+
 
 @app.route("/sosuu/")
 def index():
-  num = []
-  for i in range(10):
-    if ip(i):
-      print(i)
-      num.append(i)
-  return rndtmp('index.html',title="10までの素数", msg = num)
+    num = []
+    for i in range(10):
+        if ip(i):
+            print(i)
+            num.append(i)
+    return rndtmp('index.html', title="10までの素数", msg=num)
 
-app.run(host="127.0.0.1",port=5000)
+
+app.run(host="127.0.0.1", port=5000, debug=True)
