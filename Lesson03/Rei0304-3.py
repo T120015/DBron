c6c5c1a5b9b1cfec94e400a51af4683e34564c04
T120015 <T120015@ed.sus.ac.jp>
2021-12-23 14:49:41 +0900

    2021/12/23-14:49:40

--- Lesson03/Rei0304-3.py
+++ Lesson03/Rei0304-3.py
@@ -3,17 +3,16 @@
 import pandas as pd
 from mydblib import my_select
 
-#SQLの設定 
-# tableに検索するtable名 
-city="諏訪市"
+#SQLの設定
+# tableに検索するtable名
+city="原村"
 
 sqlstring =  f"""
     SELECT *
     FROM post_area
-    WHERE city = '{city}'
+    WHERE city = '%{city}%'
     ;
 """
 
 post_number = my_select( sqlstring )
 print( post_number ) #for debug
-
