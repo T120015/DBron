b5ce3e9680a9951b897f84d09487ae43e75fa829
T120015 <T120015@ed.sus.ac.jp>
2021-12-23 14:36:03 +0900

    2021/12/23-14:36:02

--- Lesson03/Rei0302.py
+++ Lesson03/Rei0302.py
@@ -1,6 +1,6 @@
 #例題２　seabornモジュールpairplot図
 import pandas as pd
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 #csvファイル読み込み
@@ -9,5 +9,5 @@ siken = pd.read_csv("./data/kimatsu.csv",header=0)
 #pairplot図作成
 #【入力】
 #クラスごとに色分け
-#sns.pairplot(siken, hue="Croom")
+sns.pairplot(siken, hue="Croom")
 plt.savefig("./img/Rei0302.png")
