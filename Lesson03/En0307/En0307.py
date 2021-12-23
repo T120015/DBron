9c610548c4adef80a93cc5b9e21a51001cbd3f24
T120015 <T120015@ed.sus.ac.jp>
2021-12-23 16:38:40 +0900

    2021/12/23-16:38:40

--- Lesson03/En0307/En0307.py
+++ Lesson03/En0307/En0307.py
@@ -1,5 +1,5 @@
 from flask import Flask as flk,render_template as rndtmp
-
+from sympy import sieve as sv
 
 app = flk(__name__)
 
@@ -9,7 +9,7 @@ def top():
 
 @app.route("/sosuu/")
 def index():
-
+  print([i for i in sv.primerange(10)])
   return rndtmp("index.html",title="10までの素数")
 
 app.run(host="127.0.0.1",port=5000)
