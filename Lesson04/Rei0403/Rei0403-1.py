b6bbcfaad107be8122cf9bd07b9af7e419d907b3
T120015 <T120015@ed.sus.ac.jp>
2022-01-13 15:58:28 +0900

    2022/01/13-15:58:27

--- Lesson04/Rei0403/Rei0403-1.py
+++ Lesson04/Rei0403/Rei0403-1.py
@@ -1,25 +1,30 @@
-#En0401-1　Flaskモジュールで，DBアクセス&データ分析
-#このプログラムは，グラフファイルを出力するので，実行するディレクトリが重要
-#En0401.pyが存在するディレクトリを，カレントディレクトリとして実行する
+# En0401-1　Flaskモジュールで，DBアクセス&データ分析
+# このプログラムは，グラフファイルを出力するので，実行するディレクトリが重要
+# En0401.pyが存在するディレクトリを，カレントディレクトリとして実行する
 
-from flask import Flask,render_template 
-#Flaskのコンストラクタ
-app = Flask(__name__ ,static_folder="static")
+from flask import Flask, render_template
+# Flaskのコンストラクタ
+app = Flask(__name__, static_folder="static")
 
-#ルーティング定義
+# ルーティング定義
+
+
 @app.route("/")
 def top():
-    return render_template( "【入力】",
-        title = "各地の年間平均気温",
-        message = "各地の平均気温を表示するには，http://localhost:5000/form "
-    ) 
+    return render_template("Rei0403.html",
+                           title="各地の年間平均気温",
+                           message="各地の平均気温を表示するには，http://localhost:5000/form "
+                           )
 
-#地点と年度を選択するフォーム
+# 地点と年度を選択するフォーム
+
+
 @app.route("/form")
 def top2():
-    return render_template(  "【入力】",
-        title="地域，年度選択フォーム"
-    )
+    return render_template("form.html",
+                           title="地域，年度選択フォーム"
+                           )
 
-#プログラム起動
-app.run(host="localhost",port=5000,debug=True)
+
+# プログラム起動
+app.run(host="localhost", port=5000, debug=True)
