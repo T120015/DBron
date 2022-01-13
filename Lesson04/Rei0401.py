ec1486b97572ef8ca349e9e7d9403b8bf73fafc5
T120015 <T120015@ed.sus.ac.jp>
2022-01-13 15:06:32 +0900

    2022/01/13-15:06:32

--- Lesson04/Rei0401.py
+++ Lesson04/Rei0401.py
@@ -28,21 +28,22 @@ g_none = siken.query("cram == 'none'")
 #print(f"g_eng \n{g_eng}") #for debug
 
 #手順(1)分散は等しいと仮定して良いかの検定
-from scipy.stats import bartlett
+from scipy.stats import bartlett as bt
 #b_val(検定値)，p_val(p値：有意水準　0.05未満なら分散が等しくない)
-#【入力】
+b_val, p_val = bt(g_eng, g_none)
+
 #print( f"bartlett p_value={p_val}")
 
 #手順(2)分散に応じてt検定により二つの集団の平均値に差があるか検定する
 #t検定は，scipy.statsモジュールのttest_indメソッドを使う
-from scipy.stats import ttest_ind
+from scipy.stats import ttest_ind as tt
 #平均値の検定 分散の検定により検定の種類が分かれる
 if( p_val >= 0.05):
     #分散が等しい(p_val>=0.05)のときstudentのt検定(equal_var=True)
-    #【入力】
+    b_val, p_val = tt(g_eng, g_none, equal_var=True)
 else:
     #分散が等しくない(p_val<0.05)のときweltchのt検定(equal_var=False)
-    #【入力】
+    b_val, p_val == tt(g_eng, g_none, equal_var=False)
 print( f"ttestの結果 p_value={p_val} ")
 
 #t検定をした結果，p_val>=0.05なら平均値に差がない，p_val<0.05なら平均値に差がある
