ec03786cf7f36d8472d78082d618bc6b2ae7f7e8
T120015 <T120015@ed.sus.ac.jp>
2022-01-13 17:38:50 +0900

    2022/01/13-17:38:49

--- Lesson04/En0403.py
+++ Lesson04/En0403.py
@@ -12,4 +12,17 @@ WHERE YEAR = 2019
 weather = slc("webprog", sqlstr)
 result = weather.groupby("Area").mean()
 
-print(f"{result['Temp_mean']}")
+print(
+    f"それぞれ平均:\n東京-{result['Temp_mean']['Tokyo']}, 大阪-{result['Temp_mean']['osaka']}\n")
+
+g_tokyo = weather.query("Area == 'Tokyo'")['Temp_mean']
+g_osaka = weather.query("Area == 'osaka'")['Temp_mean']
+
+b_val, p_val = bt(g_tokyo, g_osaka)
+
+if(p_val >= 0.05):
+    t_val, p_val = tt(g_tokyo, g_osaka, equal_var=True)
+else:
+    t_val, p_val = tt(g_tokyo, g_osaka, equal_var=False)
+
+print(f"ttest p_val= {p_val}")
