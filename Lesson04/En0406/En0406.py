94d79f3f49923d0b9873eae8e74aad98150fe5c0
T120015 <T120015@ed.sus.ac.jp>
2022-01-13 18:33:45 +0900

    2022/01/13-18:33:45

---
+++ Lesson04/En0406/En0406.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template as retmp, request
+from mydblib2 import my_select as slc
+
+app = Flask(__name__, static_folder="static")
+
+@app.route("/")
+def top():
+    return retmp(
+        "index.html",
+        title = "年代ごとの気象データ一元配置分散分析ページ"
+    )
