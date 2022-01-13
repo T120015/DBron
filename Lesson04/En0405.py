cc0e555cb7258bf18555e1b18bf7a58e605afd92
T120015 <T120015@ed.sus.ac.jp>
2022-01-13 18:04:46 +0900

    2022/01/13-18:04:45

---
+++ Lesson04/En0405.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from scipy.stats import f_oneway as fone
+from mydblib2 import my_select as slc
+
+sqlstr = f"""
+SELECT *
+FROM weather
+WHERE YEAR BETWEEN 2010 AND 2019
+;
+"""
+
+weather = slc("webprog", sqlstr)
+
+g_fukuoka = weather.query("Area == 'Fukuoka'")['Temp_mean']
+g_osaka = weather.query("Area == 'Osaka'")['Temp_mean']
+g_nagoya = weather.query("Area == 'Nagoya'")['Temp_mean']
+
+b_val, p_val = fone(g_fukuoka, g_osaka, g_nagoya)
+print(f"一元配置分散分析 p_val= {p_val:.3f}")
