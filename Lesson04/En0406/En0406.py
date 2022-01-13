d539544f0ea99efb1c0d2ccd2b820b47014d1576
T120015 <T120015@ed.sus.ac.jp>
2022-01-13 18:36:39 +0900

    2022/01/13-18:36:39

--- Lesson04/En0406/En0406.py
+++ Lesson04/En0406/En0406.py
@@ -3,9 +3,13 @@ from mydblib2 import my_select as slc
 
 app = Flask(__name__, static_folder="static")
 
+
 @app.route("/")
 def top():
     return retmp(
         "index.html",
-        title = "年代ごとの気象データ一元配置分散分析ページ"
+        title="年代ごとの気象データ一元配置分散分析ページ"
     )
+
+
+app.run(host="127.0.0.1", port=5000, debug=True)
