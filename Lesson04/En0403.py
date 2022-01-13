baa310366dc621f91ec8faeeb9403a7bd3cbc46a
T120015 <T120015@ed.sus.ac.jp>
2022-01-13 17:54:24 +0900

    2022/01/13-17:54:24

--- Lesson04/En0403.py
+++ Lesson04/En0403.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from scipy.stats import ttest_rel as tt, bartlett as bt
+from scipy.stats import ttest_rel as tt
 from mydblib2 import my_select as slc
 
 sqlstr = f"""
@@ -18,11 +18,6 @@ print(
 g_tokyo = weather.query("Area == 'Tokyo'")['Temp_mean']
 g_osaka = weather.query("Area == 'Osaka'")['Temp_mean']
 
-b_val, p_val = bt(g_tokyo, g_osaka)
-
-if(p_val >= 0.05):
-    t_val, p_val = tt(g_tokyo, g_osaka)
-else:
-    t_val, p_val = tt(g_tokyo, g_osaka)
+t_val, p_val = tt(g_tokyo, g_osaka)
 
 print(f"ttest p_val= {p_val}")
