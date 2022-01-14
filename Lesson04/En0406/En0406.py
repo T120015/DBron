6cf48735329b9086b22c79ae277d5bacdcdde4b7
T120015 <T120015@ed.sus.ac.jp>
2022-01-14 19:21:46 +0900

    2022/01/14-19:21:45

--- Lesson04/En0406/En0406.py
+++ Lesson04/En0406/En0406.py
@@ -58,7 +58,7 @@ def result():
         title=title,
         message=f"一元配置分散分析: p_value={p_val: .3f}",
         redata=redata,
-        cols=result.columns,
+        cols=result[data].columns,
         table_data=result.values,
         image=path
     )
